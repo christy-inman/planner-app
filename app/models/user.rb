4b0674aa9959fff0ac08faabe640c83dfbf1c5d1
github username <github email address>
2019-07-08 18:08:35 -0600

    user index file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :password_digest 
+    attr_accessor :username, :password_digest 
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: { minimum: 3 } 
     validates :password_digest, presence: true, confirmation: true, length: { in: 6..18 }

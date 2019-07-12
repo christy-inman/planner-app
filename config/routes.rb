7b15c2868fe8b342c58577ce1f676e05b2d9da79
github username <github email address>
2019-07-12 09:34:01 -0600

    cleaned out some comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :profiles
-  root 'users#index'
-
   resources :tasks
   resources :lists
+  root 'users#index'
 end
